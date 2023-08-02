522c68ff2ab7879a21ca80b394d08e68348fc8a5
azibabari <kanaazibabari@gmail.com>
2023-08-02 10:12:40 +0100

    WebSelectors.java

---
+++ Selenium Webdriver/WebSelectors.java
@@ -0,0 +1,19 @@
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WebSelectors {
+    public static void main(String [] args) throws InterruptedException{
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/login");
+        driver.findElement(By.id("username")).sendKeys("tomsmith");
+        driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
+        driver.findElement(By.className("radius")). click();
+        //alternatively, driver.findElement(By.name("name")).sendKeys("tomsmith");
+        Thread.sleep(3000);
+        driver.quit();
+    }
+}
