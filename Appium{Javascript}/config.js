a2ad3726f2caf9f64f44f055b8308f5fb61c6943
Kana Azibabari Lagalo <kanaazibabari@gmail.com>
2023-12-06 18:18:42 +0100

    Create config.js

---
+++ Appium{Javascript}/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  hostname: '192.168.1.121',
+  port: 4723,
+  path: '/wd/hub',
+  url: 'http://192.168.1.121:4723',
+  'appium:options': {
+    platformName: 'iOS',
+    platformVersion: '17.0',
+    deviceName: 'D32CA370-BFB7-478E-B92E-EFA00AE0FD0A',
+    automationName: 'Flutter',
+    app: 'application PATH on device'
+  }
+};
