ce26aad6c27f7fc2a65e4c1a37d3b27a107fc9d8
Kana Azibabari Lagalo <kanaazibabari@gmail.com>
2024-02-27 12:48:11 +0100

    Add files via upload

---
+++ ApiTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RestSharp;
+
+namespace NextBasket
+{
+    [TestClass]
+    public class ApiTest
+    {
+        [TestMethod]
+        public async Task TestMethodAsync()
+            {
+            var options = new RestClientOptions("https://simple-books-api.glitch.me")
+            {
+                MaxTimeout = -1,
+            };
+            var client = new RestClient(options);
+            var request = new RestRequest("/books", Method.Get);
+            RestResponse response = await client.ExecuteAsync(request);
+            Console.WriteLine(response.Content);
+        }
+    }
+}
